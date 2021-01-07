ab90aa643bb8ddbd33fe0739a0289835a011bf32
Sepehr-Sobhani <sobhani.sepehr@gmail.com>
2021-01-07 22:29:16 +0000

    Handle all functions for search form taken from database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -95,18 +95,56 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function (options, limit = 10) {
+  const queryParams = [];
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+    JOIN property_reviews ON properties.id = property_id
+`;
+  const addWhereOrAnd = () => (queryParams.length > 1 ? "AND" : "WHERE");
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city iLIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    queryString += `${addWhereOrAnd()} owner_id = $${queryParams.length} `;
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night * 100}`);
+    queryString += `${addWhereOrAnd()} cost_per_night > $${
+      queryParams.length
+    } `;
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night * 100}`);
+    queryString += `${addWhereOrAnd()} cost_per_night < $${
+      queryParams.length
+    } `;
+  }
+
+  queryString += `GROUP BY properties.id `;
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length}
+  `;
+
+  console.log(queryString, queryParams);
   return pool
-    .query(
-      `
-  SELECT DISTINCT *, AVG(rating) as average_rating FROM properties
-  JOIN property_reviews ON properties.id = property_id
-  GROUP BY properties.id, property_reviews.id
-  ORDER BY average_rating DESC
-  LIMIT $1
-  `,
-      [limit]
-    )
-    .then((res) => res.rows);
+    .query(queryString, queryParams)
+    .then((res) => res.rows)
+    .catch((err) => console.error(err.stack));
 };
 exports.getAllProperties = getAllProperties;
 
