ba237a5130f32898b82f484207bd8eaa748deb22
Sepehr-Sobhani <sobhani.sepehr@gmail.com>
2021-01-07 23:17:00 +0000

    Connect function to add new property with database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -140,7 +140,6 @@ const getAllProperties = function (options, limit = 10) {
   LIMIT $${queryParams.length}
   `;
 
-  console.log(queryString, queryParams);
   return pool
     .query(queryString, queryParams)
     .then((res) => res.rows)
@@ -154,9 +153,22 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryParams = Object.values(property);
+  console.log(property);
+  const queryString = `INSERT INTO properties (title, description, number_of_bedrooms, number_of_bathrooms,
+    parking_spaces,
+    cost_per_night,
+    thumbnail_photo_url,
+    cover_photo_url,
+    street,
+    country,
+    city,
+    province,
+    post_code,
+    owner_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`;
+  return pool
+    .query(queryString, queryParams)
+    .then((res) => res.rows)
+    .catch((err) => console.error(err.stack));
 };
 exports.addProperty = addProperty;
